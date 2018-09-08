1a34dd070d9cd3f5fd0e23178247af04133efa97
Andrey Dobryakov <andrey@dobryakov.biz>
2018-09-08 17:24:24 +0300

    02 string interpolation vs property binding

--- notes/02_string_interpolation.md
+++ notes/02_string_interpolation.md
@@ -2,3 +2,17 @@
 
 make a property or method in component class and use it in template
 `<h2>{{title}}</h2>` or `<h2>{{ getTitle() }}</h2>`
+
+    <!-- String interponation -->
+    <h2>{{ title }}</h2>
+    <!-- Property binding -->
+    <h2 [textContent]="title"></h2>
+    
+    <!-- String interponation -->
+    <img src="{{ imageUrl }}" />
+    <!-- Property binding -->
+    <img [src]="imageUrl" />
+    
+Important thing about property binding - it works only one way, from the component to the DOM.
+If field in the component changes, angular will update the DOM.
+But any changes in the DOM will not reflect back into the component. For example, filling input fields will not effect component.
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,7 +5,17 @@ import {EmailService} from './email.service';
 @Component({
   selector: 'app-courses', // <courses> -> "courses", <div class="courses"> -> ".courses", <div id="courses"> -> "#courses"
   template: `
+
+    <!-- String interponation -->
     <h2>{{ title }}</h2>
+    <!-- Property binding -->
+    <h2 [textContent]="title"></h2>
+    
+    <!-- String interponation -->
+    <img src="{{ imageUrl }}" />
+    <!-- Property binding -->
+    <img [src]="imageUrl" />
+    
     <ul>
       <li *ngFor="let c of courses">
         {{ c }}
@@ -15,6 +25,7 @@ import {EmailService} from './email.service';
 })
 export class CoursesComponent {
   title = 'List of courses';
+  imageUrl = 'https://picsum.photos/400/200';
   courses;
 
   constructor(coursesService: CoursesService, emailService: EmailService) {
