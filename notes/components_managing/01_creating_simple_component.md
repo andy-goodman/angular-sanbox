5e39654679e9b0117343500d4545b3bc2d111a34
Andrey Dobryakov <andrey@dobryakov.biz>
2018-09-08 15:43:42 +0300

    01 creating component by command

--- notes/components_managing/01_creating_simple_component.md
+++ notes/components_managing/01_creating_simple_component.md
@@ -2,3 +2,9 @@
 
 1. Create a component class in a module folder (example: CoursesComponent, courses.component.ts)
 2. Register it in module (example: AppModule, app.module.ts) by placing into @NgModule annotation, declarations field
+
+Another way
+
+> ng g c course
+
+In a root project directory
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {CoursesComponent} from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   // add all the comoponents of tis module
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
