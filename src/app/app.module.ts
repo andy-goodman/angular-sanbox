31aabbdc4d705c938738ce8f8bacb3620fd23552
dobryakoff <andrey@dobryakov.biz>
2018-11-19 22:09:58 +0300

    sign-up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TraversalComponent } from './transversal/traversal.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import {FormsModule, NgModel} from '@angular/forms';
+import {SignupFormComponent} from './signup-form/signup-form.component';
 
 @NgModule({
   // add all the components, pipes and directives of tis module
@@ -27,7 +28,8 @@ import {FormsModule, NgModel} from '@angular/forms';
     JustButtonComponent,
     TraversalComponent,
     InputFormatDirective,
-    ContactFormComponent
+    ContactFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
